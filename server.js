5098a3256c1943e607e55844698a6bcbe5aad369
jWat <joeljameswatson@gmail.com>
2019-10-09 16:24:58 -0700

    add variable speed control

--- server.js
+++ server.js
@@ -26,42 +26,27 @@ motorHat.init();
 const motor1 = motorHat.dcs[0];
 const motor2 = motorHat.dcs[1];
 
-function cb(err, result) {
-  // if (err) {
-  //   console.log("err: ", err);
-  // } else {
-  //   console.log("res: ", result);
-  // }
+function cb(err) {
+  if (err) {
+    console.log("error: ", err);
+  }
 }
 
 io.sockets.on("connection", socket => {
   console.log("client connected");
-  // change speed on orientation input
+
   motor1.run("fwd", cb);
   motor2.run("fwd", cb);
 
   socket.on("orientation", ({ alpha, beta, gamma }) => {
-    if (gamma < 0) {
+    // gamma = left / right (from -90 degrees tpo +90 gegrees)
+    if (gamma < 0) { // turn motors off
       motor1.setSpeed(0, cb);
       motor2.setSpeed(0, cb);
-      console.log("set speed to 0");
     } else {
       motor1.setSpeed(50, cb);
-      motor2.setSpeed(75, cb);
-      console.log("set speed to 50/75");
+      // Adjust motor2 from 0% to 90% to create different patterns
+      motor2.setSpeed(Math.round(gamma), cb);
     }
-    // if (absGamma > 0 && absGamma < 20) {
-    //   motor1.setSpeed(0, cb);
-    //   motor2.setSpeed(0, cb);
-    // } else if (absGamma > 20 && absGamma < 45) {
-    //   motor1.setSpeed(25, cb);
-    //   motor2.setSpeed(0, cb);
-    // } else if (absGamma > 45 && absGamma < 60) {
-    //   motor1.setSpeed(50, cb);
-    //   motor2.setSpeed(75, cb);
-    // } else if (absGamma > 60) {
-    //   motor1.setSpeed(50, cb);
-    //   motor2.setSpeed(100, cb);
-    // }
   });
 });
