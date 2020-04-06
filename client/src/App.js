0a542a1710a3373bb157ef4e20f67e129880ac83
Joel Watson <joeljameswatson@gmail.com>
2020-04-05 22:55:19 -0700

    behaviour / perfomance tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 let socket;
+let lastSpeed;
 
 function App(props) {
-  const [orientation, setOrientation] = useState({
-    alpha: "",
-    beta: "",
-    gamma: ""
-  });
+  // const [orientation, setOrientation] = useState({
+  //   alpha: "",
+  //   beta: "",
+  //   gamma: ""
+  // });
 
-  const [log, setLog] = useState("N/A");
   const [isActive, setIsActive] = useState(true)
 
   useEffect(() => {
@@ -21,48 +21,46 @@ function App(props) {
   }, []);
 
   function onDeviceMotion({ alpha, beta, gamma }) {
-    if(!isActive) {
-      socket.emit("orientation", { speed: 0 });
-      return
-    }
-
-    const num = gamma
+    const num = Math.round(gamma)
     let speed = 0
 
-    if (num > -90 && num < -80) {
-      // triangle
-      speed = 18
-    } else if (num > -80 && num < -65) {
-      // oval
-      speed = 48
-    } else if (num > -65 && num < 65) {
-      // stop
-      speed = 0
-    } else if (num > 65 && num < 80) {
-      //square
-      speed = 26
-    } else if (num > 80) {
-      // pentagon
-      speed = 78
+    if(isActive) {
+      if (num > -90 && num < -80) {
+        // triangle
+        speed = 18
+      } else if (num > -80 && num < -65) {
+        // oval
+        speed = 48
+      } else if (num > -65 && num < 65) {
+        // stop
+        speed = 0
+      } else if (num > 65 && num < 80) {
+        //square
+        speed = 26
+      } else if (num > 80) {
+        // pentagon
+        speed = 78
+      }
     }
 
-    setOrientation({ alpha, beta, gamma });
+    // setOrientation({ alpha, beta, gamma });
 
-    socket.emit("orientation", { speed });
+    if(speed !== lastSpeed) {
+      socket.emit("orientation", { speed });
+      lastSpeed = speed
+    }
   }
 
   return (
     <div className="App">
       <h1>Laser controller</h1>
 
-      <div>direction (alpha): {Math.round(orientation.alpha)}</div>
+      {/* <div>direction (alpha): {Math.round(orientation.alpha)}</div>
       <div>front/back (beta): {Math.round(orientation.beta)}</div>
       <div>left/right (gamma): {Math.round(orientation.gamma)}</div>
       <br />
-      <div>log: {log}</div>
-      <button onClick={() => setIsActive(!isActive)}>
-        <span>{isActive ? 'STOP' : 'START'}</span>
-      </button>
+      */}
+      <button onClick={() => setIsActive(!isActive)}>{isActive ? 'STOP' : 'START'}</button>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static("client/build"));
 const io = require("socket.io").listen(https);
 https.listen(3000, () => console.log(`listening on port: ${3000}`));
 
+let lastEvent = null;
+
 /////////  Set up motor controls ////////////
 const motorHat = require("motor-hat")({
   address: 0x60,
@@ -39,14 +41,26 @@ io.sockets.on("connection", socket => {
   motor1.run("fwd", cb);
   motor2.run("fwd", cb);
 
+  setInterval(() => {
+    if (Date.now() - lastEvent > 20000) {
+      console.log('Last event received more than 20s ago. Stopping motors...')
+      motor1.setSpeed(0, cb);
+      motor2.setSpeed(0, cb);
+    }
+  }, 10000);
+
   socket.on("orientation", ({ speed }) => {
+    console.log('speed', speed)
+
+    lastEvent = Date.now()
+
     if (!speed) {
       motor1.setSpeed(0, cb);
       motor2.setSpeed(0, cb);
     } else {
       motor1.setSpeed(50, cb);
       // Adjust motor2 from 0% to 90% to create different patterns
-      motor2.setSpeed(Math.round(speed), cb);
+      motor2.setSpeed(speed, cb);
     }
   });
 });
